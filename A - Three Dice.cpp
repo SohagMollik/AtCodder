bcc58faf830537af76ad6f11d1715bbb734c4f19
SohagMollik <sohagmollik871@gmail.com>
2021-05-23 02:27:11 +0600

    Add files via upload

---
+++ A - Three Dice.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll    long long
+#define pb    push_back
+#define ull   unsigned long long
+#define f     first
+#define s     second
+#define mod   1000000007
+#define pi    acos(-1.0)
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main()
+{
+    fast;
+     int x,y,z;
+     cin>>x>>y>>z;
+     cout<<21-(x+y+z)<<endl;
+
+
+    return 0;
+}
